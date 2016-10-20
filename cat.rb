dfa868c4203ab23a5c33f5248025ccb9b20fe87b
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-20 14:19:59 -0400

    Made two instances

--- cat.rb
+++ cat.rb
@@ -1,8 +1,14 @@
+#Create a class called cat.
 class Cat
 
+#Create an initialize method
   def initialize(name, preferred_food, meal_time)
     @name           = name
     @preferred_food = preferred_food
     @meal_time      = meal_time
   end
 end
+
+#Create two new instances
+fluffy = Cat.new('Fluff', 'Raw fish', 5)
+hobbes = Cat.new('Hobbes', 'Peanut butter samvich', 3)
